b0af7b0f3534a33480515f98172a5e6efdf6ec68
Glenelly <glenellykarlatbm@gmail.com>
2023-07-04 16:10:41 -0300

    Criando Classes	e Atributos - Carro

---
+++ src/main/java/com/example/exerciciosjavapoo/ClassesEAtributos/Carro/Carro.java
@@ -0,0 +1,9 @@
+package com.example.exerciciosjavapoo.ClassesEAtributos.Carro;
+
+public class Carro {
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+}
