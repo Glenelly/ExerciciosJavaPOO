e49b6cd13c3961e78877dac9e58180f844b54734
Glenelly <glenellykarlatbm@gmail.com>
2023-07-09 12:50:29 -0300

    Criando Classes	e Metodos - Ligar e desligar lampada

---
+++ src/main/java/com/example/exerciciosjavapoo/ClassesEMetodos/ExercicioLampada/Lampada.java
@@ -0,0 +1,31 @@
+package com.example.exerciciosjavapoo.ClassesEMetodos.ExercicioLampada;
+
+public class Lampada {
+    boolean ligada;
+
+    void ligar(){
+        ligada = true;
+    }
+
+    void  desligar(){
+        ligada = false;
+    }
+
+    void mostrarEstado(){
+        if(ligada){
+            System.out.println("Lâmpada está ligada");
+        }else{
+            System.out.println("Lâmpada está desligada");
+        }
+    }
+
+    void mudarEstado(){
+        if (ligada){
+            desligar();
+        }else {
+            ligar();
+        }
+    }
+
+
+}
