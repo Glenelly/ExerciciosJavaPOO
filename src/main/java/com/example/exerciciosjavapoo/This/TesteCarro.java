b0bdd04be16554184b8be73cc21dff33461a1aee
Glenelly <glenellykarlatbm@gmail.com>
2023-07-13 14:07:17 -0300

    Criando This

---
+++ src/main/java/com/example/exerciciosjavapoo/This/TesteCarro.java
@@ -0,0 +1,8 @@
+package com.example.exerciciosjavapoo.This;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+        Carro van = new Carro("Fiat", "Ducato");
+        System.out.println(van.numPassageiros);
+    }
+}
