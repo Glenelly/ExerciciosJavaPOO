3d5171ebb1f8b6f78667e0e9596f33ab7b61cf34
Glenelly <glenellykarlatbm@gmail.com>
2023-07-15 17:25:43 -0300

    Exercicio Variáveis e metodos estáticos - Contador

---
+++ src/main/java/com/example/exerciciosjavapoo/estatico/contador/TesteContador.java
@@ -0,0 +1,30 @@
+package com.example.exerciciosjavapoo.estatico.contador;
+
+public class TesteContador {
+
+    static void imprimirValor(){
+        System.out.println(Contador.obterValor());
+    }
+    public static void main(String[] args) {
+
+        imprimirValor();
+
+        Contador.incrementar();
+        imprimirValor();
+
+        Contador.zerar();
+        imprimirValor();
+
+        Contador.incrementar();
+        Contador.incrementar();
+        Contador.incrementar();
+        imprimirValor();
+
+        Contador c1 = new Contador();
+        Contador c2 = new Contador();
+        Contador c3 = new Contador();
+
+        imprimirValor();
+
+    }
+}
