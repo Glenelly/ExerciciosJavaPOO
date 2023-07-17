3f610dabe767a08739604e9240b4737c88ea0c0d
Glenelly <glenellykarlatbm@gmail.com>
2023-07-17 18:16:00 -0300

    Recursividade - Fibonacci

---
+++ src/main/java/com/example/exerciciosjavapoo/recursividade/fibonacci/TesteFibonacci.java
@@ -0,0 +1,10 @@
+package com.example.exerciciosjavapoo.recursividade.fibonacci;
+
+public class TesteFibonacci {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++){
+            System.out.print(Fibonacci.fibonacci(i) + " ");
+        }
+    }
+}
