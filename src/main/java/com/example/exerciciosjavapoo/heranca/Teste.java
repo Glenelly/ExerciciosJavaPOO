e8bb6c7b7091cafef477a85a0ac0bde68faefa58
Glenelly <glenellykarlatbm@gmail.com>
2023-07-20 18:14:59 -0300

    Herança

---
+++ src/main/java/com/example/exerciciosjavapoo/heranca/Teste.java
@@ -0,0 +1,19 @@
+package com.example.exerciciosjavapoo.heranca;
+
+public class Teste {
+    public static void main(String[] args) {
+
+       /* Aluno aluno = new Aluno();
+        aluno.setNome("Maria");
+
+        Professor professor = new Professor();
+        aluno.setNome("João");*/
+
+        Pessoa aluno = new Aluno();
+        //aluno.get
+
+        Pessoa professor = new Professor();
+        //professor.get
+
+    }
+}
