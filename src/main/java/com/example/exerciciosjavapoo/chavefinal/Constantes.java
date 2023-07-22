140b386682c08711ead387ab5d2fc4105fe6730a
Glenelly <glenellykarlatbm@gmail.com>
2023-07-22 15:42:12 -0300

    Palavra chave final

---
+++ src/main/java/com/example/exerciciosjavapoo/chavefinal/Constantes.java
@@ -0,0 +1,7 @@
+package com.example.exerciciosjavapoo.chavefinal;
+
+public class Constantes {
+
+    public static final String URL_BLOG = "https://glenelly.com";
+    public static final String CURSO_CLOMPLETO = "https://glenelly.training";
+}
---
+++ src/main/java/com/example/exerciciosjavapoo/chavefinal/Teste.java
@@ -0,0 +1,17 @@
+package com.example.exerciciosjavapoo.chavefinal;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa();
+        //Pessoa aluno = new Aluno();
+        //Pessoa professor = new Professor();
+
+        Aluno aluno = new Aluno();
+        Professor professor = new Professor();
+
+        System.out.println(Constantes.URL_BLOG);
+        System.out.println(Constantes.CURSO_CLOMPLETO);
+
+    }
+}
